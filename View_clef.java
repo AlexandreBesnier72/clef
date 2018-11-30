bef8dd147f12d4272368c3d97c84f305c52b17cb
Alexandre <alexandre-besnier@live.fr>
2018-11-30 20:50:53 +0100

    Creation methods "saisie + attribute name" and "update"
    Création de méthodes du style saisie + nom de l'attribut et création de la méthode "update" qui permet de faire saisir à l'utilisateur de resaisir un attribut d'une des clefs

--- View_clef.java
+++ View_clef.java
@@ -8,41 +8,6 @@ public class View_clef
 {
     private Scanner sc = new Scanner( System.in );
 
-    public int mainMenu()
-    {
-        // saisie
-        int saisie;
-
-        // saisie d'attribut de clef
-        String saisieId;
-        String saisieProprio;
-        String saisiePorte;
-        String saisieMarque;
-        char saisieTechno;
-        String saisieMatiere;
-        boolean saisieDispo = false;
-
-        char saisieChar;
-        String saisieValue;
-
-        // variable d'erreur
-        boolean error = false;
-
-        System.out.println( "Que voulez-vous faire ?" );
-        System.out.println( "[1] ajouter une clé." );
-        System.out.println( "[2] supprimer une clé." );
-        System.out.println( "[3] mettre à jour une clé." );
-        System.out.println( "[4] faire une recherche." );
-        System.out.println( "[5] afficher la liste des clefs." );
-        System.out.println( "[6] afficher une clefs précise." );
-        System.out.println( "[7] sortir du programme." );
-
-        saisie = sc.nextInt();
-        sc.nextLine(); // debug
-
-        return saisie;
-    }
-
     private boolean errorProprio( String saisie )
     {
         boolean error = false;
@@ -55,6 +20,7 @@ public class View_clef
 
         return error;
     }
+
     private boolean errorMarque( String saisie )
     {
         boolean error = false;
@@ -80,6 +46,107 @@ public class View_clef
         return error;
     }
 
+    private int saisieMenu()
+    {
+        int saisie;
+
+        saisie = sc.nextInt();
+        sc.nextLine(); // debug
+
+        return saisie;
+    }
+
+    private String saisieId()
+    {
+        String saisie;
+
+        System.out.println( "Qu'elle est l'id de la clé à changer ?" );
+        saisie = sc.nextLine();
+
+        return saisie;
+    }
+
+    private String saisiePropio()
+    {
+        String saisie;
+
+        System.out.println( "Qu'elle est le nom du propriétaire ? ( min 4 caractères )" );
+        saisie = sc.nextLine();
+
+        return saisie;
+    }
+
+    private String saisiePorte()
+    {
+        String saisie;
+
+        System.out.println( "Qu'elle est le nom de la porte ?" );
+        saisie = sc.nextLine();
+
+        return saisie;
+    }
+
+    private String saisieMarque()
+    {
+        String saisie;
+
+        System.out.println( "Qu'elle est le nom de la marque ?" );
+        saisie = sc.nextLine();
+
+        return saisie;
+    }
+
+    private char saisieTechno()
+    {
+        char saisie;
+
+        System.out.println( "Qu'elle technologie utilise la clef ? [A/E]");
+        saisie = sc.next().toLowerCase().charAt(0);
+        sc.nextLine(); // debug
+
+        return saisie;
+    }
+
+    private String saisieMatiere()
+    {
+        String saisie;
+
+        System.out.println( "De quelle matière est faite la clef ?" );
+        saisie = sc.nextLine();
+
+        return saisie;
+    }
+
+    private char saisieDispo()
+    {
+        char saisie;
+
+        System.out.println( "La clé est-elle dispo ? [O/N]" );
+        saisie = sc.next().toLowerCase().charAt(0);
+        sc.nextLine(); // debug
+
+        return saisie;
+    }
+
+    public int mainMenu()
+    {
+        // saisie
+        int saisie;
+
+        System.out.println( "Que voulez-vous faire ?" );
+        System.out.println( "[1] ajouter une clé." );
+        System.out.println( "[2] supprimer une clé." );
+        System.out.println( "[3] mettre à jour une clé." );
+        System.out.println( "[4] faire une recherche." );
+        System.out.println( "[5] afficher la liste des clefs." );
+        System.out.println( "[6] afficher une clefs précise." );
+        System.out.println( "[7] sortir du programme." );
+
+        saisie = this.saisieMenu();
+
+        return saisie;
+    }
+
     public Iterator add()
     {
         // Objet de stockage des saisies
@@ -102,25 +169,12 @@ public class View_clef
 
         do
         {
-            System.out.println( "Qu'elle est le nom du propriétaire ? ( min 4 caractères )" );
-            saisieProprio = sc.nextLine();
-
-            System.out.println( "Qu'elle est le nom de la porte ?" );
-            saisiePorte = sc.nextLine();
-
-            System.out.println( "Qu'elle est le nom de la marque ? ( min 2 caractères )" );
-            saisieMarque = sc.nextLine();
-
-            System.out.println( "Qu'elle technologie utilise la clef ? [A/E]");
-            saisieTechno = sc.next().toLowerCase().charAt(0);
-            sc.nextLine(); // debug
-
-            System.out.println( "De quelle matière est faite la clef ?" );
-            saisieMatiere = sc.nextLine();
-
-            System.out.println( "La clé est-elle dispo ? [O/N]" );
-            saisieDispo = sc.next().toLowerCase().charAt(0);
-            sc.nextLine(); // debug
+            saisieProprio = this.saisiePropio();
+            saisiePorte = this.saisiePorte();
+            saisieMarque = this.saisieMarque();
+            saisieTechno = this.saisieTechno();
+            saisieMatiere = this.saisieMatiere();
+            saisieDispo = this.saisieDispo();
 
             if ( saisieDispo == 'o')
             {
@@ -165,5 +219,97 @@ public class View_clef
         saisieId = sc.next();
 
         return saisieId;
+    }
+
+    public Iterator update()
+    {
+        // Objet de stockage des saisies
+        ArrayList<Object> saisie = new ArrayList<>();
+
+        // objet de retour
+        Iterator updateClef;
+
+        // variable d'erreur
+        boolean error;
+
+        int saisieMenu;
+        String saisieId;
+        String saisieProprio;
+        String saisiePorte;
+        String saisieMarque;
+        char saisieTechno;
+        String saisieMatiere;
+        boolean saisieDispo = false;
+
+        do
+        {
+            error = false;
+
+            saisieId = this.saisieId();
+            saisie.add(saisieId);
+
+            System.out.println( "Quelle est le nom du champs à modifier ?" );
+            System.out.print( "[1] Propriétaire - " );
+            System.out.print( "[2] Numéro de la porte - " );
+            System.out.print( "[3] Marque - " );
+            System.out.print( "[4] Technologie - " );
+            System.out.println( "[5] La disponibilité" );
+
+            saisieMenu = this.saisieMenu();
+
+            switch ( saisieMenu )
+            {
+                // proprio
+                case 1 :
+                    do
+                    {
+                        saisieProprio = this.saisiePropio();
+                    } while ( saisieProprio.length() < 4 );
+
+                    saisie.add("propriétaire");
+                    saisie.add(saisieProprio);
+                    break;
+
+                // porte
+                case 2 :
+                    saisie.add("porte");
+                    saisie.add( this.saisiePorte() );
+                    break;
+
+                // marque
+                case 3 :
+                    do
+                    {
+                        saisieMarque = this.saisieMarque();
+                    } while( saisieMarque.length() < 2 );
+
+                    saisie.add("marque");
+                    saisie.add(saisieMarque);
+                    break;
+
+                // techno
+                case 4 :
+                    do
+                    {
+                        saisieTechno = this.saisieTechno();
+                    } while ( saisieTechno != 'a' && saisieTechno != 'e' );
+
+                    saisie.add("technologie");
+                    saisie.add(saisieTechno);
+                    break;
+
+                // disponibilité
+                case 5 :
+                    saisie.add("dispo");
+                    break;
+
+                default :
+                    System.out.println( "Erreur de saisie" );
+            }
+        } while ( error );
+
+        updateClef = saisie.iterator();
+
+        return updateClef;
     }
 }
